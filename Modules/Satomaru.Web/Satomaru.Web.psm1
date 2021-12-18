dd246bd498e8ddd39f535326552d12b3b213c9fd
Satomaru <erynenadan@gmail.com>
2021-12-19 00:14:49 +0900

    classを外部ファイルにしてusing moduleするのをやめました。色々と都合が悪い……。

--- Modules/Satomaru.Web/Satomaru.Web.psm1
+++ Modules/Satomaru.Web/Satomaru.Web.psm1
@@ -1,7 +1,94 @@
-﻿using module ".\ContentTypeInfo.psm1"
-using module ".\UriInfo.psm1"
+﻿Import-Module -Name Satomaru.Util
 
-Import-Module -Name Satomaru.Util
+$Script:Specs = @{
+    "*/*"                          = @{ AsText = $false; Extensions = @("dat", "*");                  Charset = "ISO-8859-1" }
+    "application/gzip"             = @{ AsText = $false; Extensions = @("gz");                        Charset = "ISO-8859-1" }
+    "application/java-archiver"    = @{ AsText = $false; Extensions = @("jar");                       Charset = "ISO-8859-1" }
+    "application/json"             = @{ AsText = $true;  Extensions = @("json");                      Charset = "ISO-8859-1" }
+    "application/octet-stream"     = @{ AsText = $false; Extensions = @("dat", "*");                  Charset = "ISO-8859-1" }
+    "application/pdf"              = @{ AsText = $false; Extensions = @("pdf");                       Charset = "ISO-8859-1" }
+    "application/zip"              = @{ AsText = $false; Extensions = @("zip");                       Charset = "ISO-8859-1" }
+    "application/x-gzip"           = @{ AsText = $false; Extensions = @("gz");                        Charset = "ISO-8859-1" }
+    "application/x-zip-compressed" = @{ AsText = $false; Extensions = @("zip");                       Charset = "ISO-8859-1" }
+    "audio/mpeg"                   = @{ AsText = $false; Extensions = @("mp3");                       Charset = "ISO-8859-1" }
+    "audio/wav"                    = @{ AsText = $false; Extensions = @("wav");                       Charset = "ISO-8859-1" }
+    "audio/x-mpeg"                 = @{ AsText = $false; Extensions = @("mp3");                       Charset = "ISO-8859-1" }
+    "audio/x-wav"                  = @{ AsText = $false; Extensions = @("wav");                       Charset = "ISO-8859-1" }
+    "image/bmp"                    = @{ AsText = $false; Extensions = @("bmp");                       Charset = "ISO-8859-1" }
+    "image/gif"                    = @{ AsText = $false; Extensions = @("gif");                       Charset = "ISO-8859-1" }
+    "image/jpeg"                   = @{ AsText = $false; Extensions = @("jpg", "jpeg");               Charset = "ISO-8859-1" }
+    "image/png"                    = @{ AsText = $false; Extensions = @("png");                       Charset = "ISO-8859-1" }
+    "image/svg+xml"                = @{ AsText = $true;  Extensions = @("svg");                       Charset = "UTF-8" }
+    "image/x-bmp"                  = @{ AsText = $false; Extensions = @("bmp");                       Charset = "ISO-8859-1" }
+    "image/x-ms-bmp"               = @{ AsText = $false; Extensions = @("bmp");                       Charset = "ISO-8859-1" }
+    "image/x-png"                  = @{ AsText = $false; Extensions = @("png");                       Charset = "ISO-8859-1" }
+    "text/*"                       = @{ AsText = $true;  Extensions = @("txt", "*");                  Charset = "ISO-8859-1" }
+    "text/css"                     = @{ AsText = $true;  Extensions = @("css");                       Charset = "ISO-8859-1" }
+    "text/csv"                     = @{ AsText = $true;  Extensions = @("csv");                       Charset = "ISO-8859-1" }
+    "text/html"                    = @{ AsText = $true;  Extensions = @("html", "htm");               Charset = "UTF-8" }
+    "text/javascript"              = @{ AsText = $true;  Extensions = @("js");                        Charset = "ISO-8859-1" }
+    "text/plain"                   = @{ AsText = $true;  Extensions = @("txt");                       Charset = "ISO-8859-1" }
+    "video/quicktime"              = @{ AsText = $true;  Extensions = @("mov", "moov", "qt");         Charset = "ISO-8859-1" }
+    "video/mp4"                    = @{ AsText = $false; Extensions = @("mp4");                       Charset = "ISO-8859-1" }
+    "video/mpeg"                   = @{ AsText = $false; Extensions = @("mpeg");                      Charset = "ISO-8859-1" }
+    "video/x-mpeg"                 = @{ AsText = $false; Extensions = @("mpeg", "mpg", "mpe", "mpv"); Charset = "ISO-8859-1" }
+    "vide/x-msvideo"               = @{ AsText = $false; Extensions = @("avi");                       Charset = "ISO-8859-1" }
+}
+
+class ContentTypeInfo {
+    [string] $ContentType
+    [string[]] $Elements
+    [hashtable] $Attributes
+    [string] $Charset
+    [boolean] $AsText
+    [string[]] $Extentions
+
+    ContentTypeInfo([string] $ContentType) {
+        $this.ContentType = $ContentType
+        $this.Elements = ($ContentType -split ";").Trim()
+        $this.Attributes = ConvertTo-Hashtable -Target $ContentType -ED ";" -PS "="
+        $Spec = $Script:Specs[$this.Elements[0]] ?? $Script:Specs[($ContentType.StartsWith("text/")) ? "text/*" : "*/*"]
+        $this.Charset = $this.Attributes.charset ?? $Spec.Charset
+        $this.AsText = $Spec.AsText
+        $this.Extentions = $Spec.Extensions
+    }
+
+    [boolean] MatchExtension([string] $Extension) {
+        if ($null -eq $Extension -or $Extension -eq "") {
+            return $false
+        }
+
+        return $this.Extentions -contains "*" -or $this.Extentions -contains $Extension
+    }
+
+    [string] GetExtention() {
+        return $this.Extentions[0]
+    }
+}
+
+class UriInfo {
+    [Uri] $Uri
+    [string] $FileName
+    [string] $BaseName
+    [string] $Extension
+
+    UriInfo([Uri] $Uri) {
+        $this.Uri = $Uri
+        $this.FileName = [System.IO.Path]::GetFileName($Uri.LocalPath)
+        $this.BaseName = [System.IO.Path]::GetFileNameWithoutExtension($Uri.LocalPath)
+        $this.Extension = [System.IO.Path]::GetExtension($Uri.LocalPath)
+        $this.Extension = $this.Extension.StartsWith(".") ? $this.Extension.Substring(1) : $this.Extension
+    }
+
+    [string] GetFileNameOrAlter([string] $BaseNameWhenEmpty, [ContentTypeInfo] $ContentTypeInfo) {
+        if ($ContentTypeInfo.MatchExtension($this.Extension)) {
+            return $this.FileName
+        }
+
+        [string] $Alternated = ($this.BaseName -ne "") ? $this.BaseName : $BaseNameWhenEmpty
+        return $Alternated + "." + $ContentTypeInfo.GetExtention()
+    }
+}
 
 class SaveInfo {
     [Uri] $RequestUri
@@ -33,7 +120,7 @@ function Save-WebResponse {
         $Info.FileName = if ($FileName -ne "") {
             $FileName
         } else {
-            [UriInfo]::new($Info.RequestUri).GetFileNameOrAlter("response<n>", $ContentTypeInfo.Extentions)
+            [UriInfo]::new($Info.RequestUri).GetFileNameOrAlter("response<n>", $ContentTypeInfo)
         }
 
         if ($Info.FileName.Contains("<n>")) {
