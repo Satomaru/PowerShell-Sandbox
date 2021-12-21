3757e5ac3488592ce90872dc459c9f5b48731fa4
STB a.k.a. Satomaru <Satomaru@users.noreply.github.com>
2021-12-21 13:15:41 +0900

    Search-ByNameはPowerShellの命名規則に合ってないと思われるので、改名。

--- Modules/Satomaru.FileSystem/Satomaru.FileSystem.psm1
+++ Modules/Satomaru.FileSystem/Satomaru.FileSystem.psm1
@@ -1,18 +1,44 @@
 #Requires -Version 7
 using namespace Microsoft.PowerShell.Commands
 
-function Search-ByName {
+function Search-Item {
     [OutputType([System.IO.FileSystemInfo])]
+
     Param(
-        [Parameter(Mandatory, ValueFromPipeline)] [System.IO.FileSystemInfo] $Target,
-        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()] [string] $Pattern
+        [Parameter(Mandatory, ValueFromPipeline)] [System.IO.FileSystemInfo] $Item,
+        [ValidateNotNull()] [string] $ByName,
+        [ValidateNotNull()] [string] $ByContent,
+        [FileSystemCmdletProviderEncoding] $Encoding
     )
 
     Process {
-        if ($target.Name -match $Pattern) {
-            return $Target
+        if ($ByName -ne "") {
+            if ($Item.Name -notmatch $ByName) {
+                return
+            }
         }
+
+        if ($ByContent -ne "") {
+            if ($null -ne $Encoding) {
+                $Encoding = [FileSystemCmdletProviderEncoding]::Ascii
+            }
+
+            [boolean] $Hit = $false
+
+            foreach ($Content in $Item | Get-Content -Encoding $Encoding) {
+                if ($Content -match $ByContent) {
+                    $Hit = $true
+                    break
+                }
+            }
+
+            if (-not $Hit) {
+                return
+            }
+        }
+
+        return $Item
     }
 }
 
-Export-ModuleMember -Function Search-ByName
+Export-ModuleMember -Function Search-Item
