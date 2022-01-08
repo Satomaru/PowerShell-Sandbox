bab6b866bf71f2062290caf4b0a6b6326f935d57
Satomaru <erynenadan@gmail.com>
2022-01-08 10:39:53 +0900

    Satomaru.Utilを一旦仕上げた。

--- Modules/Satomaru.Util/Satomaru.Util.psd1
+++ Modules/Satomaru.Util/Satomaru.Util.psd1
@@ -2,6 +2,7 @@
     RootModule = 'Satomaru.Util.psm1'
     ModuleVersion = '1.0.0.0'
     Author = 'Satomaru'
+    Description = '全てのモジュールの土台となるユーティリティです。'
     PowerShellVersion = '7.2'
     FunctionsToExport = @('Find-Object', 'Optimize-String', 'Split-Parameter', 'Test-Array')
 }
--- Modules/Satomaru.Util/Satomaru.Util.psm1
+++ Modules/Satomaru.Util/Satomaru.Util.psm1
@@ -1,61 +1,63 @@
 <#
     .SYNOPSIS
-    オブジェクトを抽出する。
+    オブジェクトを抽出します。
 
     .DESCRIPTION
-    オブジェクトを受け取って検査を行う。
-    検査に合格した場合は、受け取ったオブジェクトをそのまま返却する。
-    なお、null の場合は、その時点で不合格とする。
+    オブジェクトを受け取り、 期待する条件に一致することを検査します。
+    期待する条件に一致する場合は、そのまま返却します。
 
     .PARAMETER Target
-    検査するオブジェクト。
+    オブジェクト。
 
     .PARAMETER Property
     オブジェクトの検査対象となるプロパティ名。
-    指定しなかった場合は、オブジェクトそのものが検査される。
+    指定しなかった場合は、オブジェクトそのものが検査されます。
 
     .PARAMETER Truthy
-    true と解釈されることを検査する。
+    trueと解釈できることを期待します。
 
     .PARAMETER Falsy
-    false と解釈されることを検査する。
+    falseと解釈できることを期待します。
 
     .PARAMETER EQ
-    指定値と等しいことを検査する。
+    指定値と等しいことを期待します。
 
     .PARAMETER NE
-    指定値と異なることを検査する。
+    指定値と異なることを期待します。
 
     .PARAMETER LT
-    指定値よりも小さいことを検査する。
+    指定値よりも小さいことを期待します。
 
     .PARAMETER LE
-    指定値よりも小さいか等しいことを検査する。
+    指定値よりも小さいか等しいことを期待します。
 
     .PARAMETER GT
-    指定値よりも大きいことを検査する。
+    指定値よりも大きいことを期待します。
 
     .PARAMETER GE
-    指定値よりも大きいか等しいことを検査する。
+    指定値よりも大きいか等しいことを期待します。
 
     .PARAMETER Contains
-    指定値のうちのいずれか一つと等しいことを検査する。
+    指定値のうちのいずれか一つと等しいことを期待します。
 
     .PARAMETER Match
-    指定された正規表現に一致することを検査する。
+    指定された正規表現に一致することを期待します。
 
     .INPUTS
-    検査するオブジェクト。
+    オブジェクト。
 
     .OUTPUTS
-    検査に合格した場合は、検査したオブジェクト。
+    object
+    期待する条件に一致する場合は、入力されたオブジェクト。
 
     .EXAMPLE
-    @(1,2,3,4,5) | Find-Object -GE 2 -LE 4
+    1..5 | Find-Object -GE 2 -LE 4
+
     2, 3, 4 が抽出される。
 
     .EXAMPLE
     Get-Item *.txt | Find-Object -Property Length -LE 70
+
     ファイルサイズが 70 byte 以下の *.txt が抽出される。
 #>
 function Find-Object {
@@ -87,7 +89,7 @@ function Find-Object {
             | Where-Object { $null -eq $LE -or $_ -le $LE } `
             | Where-Object { $null -eq $GT -or $_ -gt $GT } `
             | Where-Object { $null -eq $GE -or $_ -ge $GE } `
-            | Where-Object { -not $Contains -or $Contains.Contains($_) } `
+            | Where-Object { -not $Contains -or $_ -in $Contains } `
             | Where-Object { -not $Match -or $_ -match $Match } `
             | ForEach-Object { $Target }
     }
@@ -95,13 +97,13 @@ function Find-Object {
 
 <#
     .SYNOPSIS
-    文字列を最適化する。
+    文字列を最適化します。
 
     .DESCRIPTION
-    文字列の前後にあるホワイトスペースを除去した後、クォートを解除する。
+    文字列の前後にあるホワイトスペースを除去した後、クォートを解除します。
     クォートの解除は、先頭と末尾がともにダブルクォート、
-    またはともにシングルクォートの場合のみ行う。
-    
+    またはともにシングルクォートの場合のみ行います。
+
     .PARAMETER Target
     最適化する文字列。
 
@@ -113,7 +115,8 @@ function Find-Object {
 
     .EXAMPLE
     " 'foo ' " | Optimize-String
-    "foo " が返却される。
+
+    "foo " が返却されます。
 #>
 function Optimize-String {
     [OutputType([string])]
@@ -137,11 +140,16 @@ function Optimize-String {
 
 <#
     .SYNOPSIS
-    パラメータ文字列を要素毎に分割する。
+    パラメータ文字列を式毎に分割します。
 
     .DESCRIPTION
-    まずパラメータ文字列をパーツ区切り文字で分割する。
-    分割したパーツがプロパティ式の書式になってい場合は、さらに名前と値に分割する。
+    パラメータ文字列とは、以下のような文字列を意味します。
+    "Foo = 1; Bar = 2; Baz"
+
+    まずパラメータ文字列を、パーツ区切り文字（上記例では";"）で分割します。
+    分割したパーツがプロパティ式になっている場合（上記例ではFooとBar）は、
+    さらに名前と値に分割され、引数のhashtableに格納されます。
+    なお、名前と値は、必ずstringになります。
 
     .PARAMETER Parameter
     パラメータ文字列。
@@ -150,20 +158,21 @@ function Optimize-String {
     プロパティ式の名前と値を格納する、ハッシュテーブルの参照。
 
     .PARAMETER PartsDelimiter
-    別名 "PD"。パーツ区切り文字。省略時は ";" を用いる。
+    パーツ区切り文字。省略時は ";" を用います。
 
     .PARAMETER ValueSeparator
-    別名 "VS"。プロパティ式の値区切り文字。省略時は "=" を用いる。
+    プロパティ式の値区切り文字。省略時は "=" を用います。
     
     .INPUTS
     パラメータ文字列。
 
     .OUTPUTS
-    パーツ区切り文字で分割された、パーツの配列。
+    分割されたパーツの配列。
 
     .EXAMPLE
     [string[]] $Parts = "text/html; charset=UTF-8" | Split-Parameter -Values ([ref] $Values)
-    $Parts には @("text/html", "charset=UTF-8") が、$Props には @{charset = "UTF-8"} が格納される。
+
+    $Parts には @("text/html", "charset=UTF-8") が、$Props には @{charset = "UTF-8"} が格納されます。
 #>
 function Split-Parameter {
     [OutputType([string[]])]
@@ -171,8 +180,8 @@ function Split-Parameter {
     Param(
         [Parameter(Mandatory, ValueFromPipeline)] [string] $Parameter,
         [hashtable] [ref] $Values,
-        [Alias("PD")] [string] $PartsDelimiter = ";",
-        [Alias("VS")] [string] $ValueSeparator = "="
+        [Alias("PD")] [ValidateNotNullOrEmpty()] [string] $PartsDelimiter = ";",
+        [Alias("VS")] [ValidateNotNullOrEmpty()] [string] $ValueSeparator = "="
     )
 
     Process {
@@ -194,10 +203,10 @@ function Split-Parameter {
 
 <#
     .SYNOPSIS
-    配列が期待どおりであることを検査する。
+    配列が期待どおりであることを検査します。
 
     .DESCRIPTION
-    配列の長さ、および各要素の値が等しい場合は true を返却する。
+    配列の長さ、および各要素の値が等しい場合は$trueを返却します。
 
     .PARAMETER Actual
     検査対象となる配列の参照。
@@ -209,11 +218,13 @@ function Split-Parameter {
     なし。
 
     .OUTPUTS
-    判定結果。
+    boolean
+    配列の長さ、および各要素の値が等しい場合は$true。
 
     .EXAMPLE
     Test-Array -Actual ([ref] $foo) -Expected ([ref] $bar)
-    $foo と $bar の要素数、および各要素の値が等しい場合は、true が返却される。
+
+    $fooと$barの要素数、および各要素の値が等しい場合は、$rueが返却されます。
 #>
 function Test-Array {
     [OutputType([boolean])]
